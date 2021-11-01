88069d767091250c69813c31899ac316ae80e5d9
Ian McPhail <ian.mcphail@thinkcompany.com>
2021-11-01 10:36:00 -0400

    Add getData function

--- static.config.js
+++ static.config.js
@@ -1,6 +1,8 @@
 import path from 'path';
 import React from 'react';
 
+import { getBase, getFields } from './src/helpers';
+
 // This file is used to configure:
 // - static-site generation
 // - Document shell (index.html)
@@ -21,9 +23,58 @@ const config = {
     </Html>
   ),
   getRoutes: async () => {
+    const countdownBase = await getBase('📆 Schedule')
+      .select({ maxRecords: 1 })
+      .firstPage();
+    const [countdownField] = getFields(countdownBase);
+    const dayOneBase = await getBase('📆 Schedule')
+      .select({ view: 'Day One schedule' })
+      .firstPage();
+    const dayTwoBase = await getBase('📆 Schedule')
+      .select({ view: 'Day Two schedule' })
+      .firstPage();
+    const schedule = [
+      {
+        id: 'dayOne',
+        title: 'Day One',
+        subtitle: 'February 18',
+        events: getFields(dayOneBase)
+      },
+      {
+        id: 'dayTwo',
+        title: 'Day Two',
+        subtitle: 'February 19',
+        events: getFields(dayTwoBase)
+      }
+    ];
+    const speakersBase = await getBase('🎤 Speakers')
+      .select({
+        view: 'All speakers',
+        fields: ['Name', 'Role', 'Company', 'Image'],
+        filterByFormula: 'AND({Confirmed?}, {Speaking at})'
+      })
+      .firstPage();
+    const speakers = getFields(speakersBase);
+    const sponsorsBase = await getBase('💸 Sponsors').select().firstPage();
+    const sponsors = sponsorsBase.map(record => {
+      return {
+        name: record.fields.Name,
+        image: record.fields.Image[0].url,
+        url: record.fields.URL
+      };
+    });
+
     return [
       {
-        path: '/'
+        path: '/',
+        getData: () => {
+          return {
+            startDate: countdownField.start,
+            schedule,
+            speakers,
+            sponsors
+          };
+        }
       }
     ];
   },
